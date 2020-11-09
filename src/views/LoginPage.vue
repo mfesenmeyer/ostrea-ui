9b4e22971ce6d33a190e8b163725c06546faaefc
Mark Fesenmeyer <markfesenmeyer@Marks-MacBook-Air.local>
2020-11-08 21:09:35 -0500

    added hidden files to root

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,24 +1,22 @@
 <template>
-  <div>
-    <h2>Login</h2>
-    <form>
-      <div class="form-group">
-        <label for="username">Username</label>
-        <input />
-      </div>
-      <div class="form-group">
-        <label htmlFor="password">Password</label>
-        <input />
-        <div>Password is required</div>
-      </div>
-      <div class="form-group">
-        <button class="btn btn-primary">Login</button>
-        <router-link to="/register" class="btn btn-link">
-          Register
-        </router-link>
-      </div>
-    </form>
-  </div>
+	<div>
+		<h2>Login</h2>
+		<form>
+			<div class="form-group">
+				<label for="username">Username</label>
+				<input />
+			</div>
+			<div class="form-group">
+				<label htmlFor="password">Password</label>
+				<input />
+				<div>Password is required</div>
+			</div>
+			<div class="form-group">
+				<button class="btn btn-primary">Login</button>
+				<router-link to="/register" class="btn btn-link"> Register </router-link>
+			</div>
+		</form>
+	</div>
 </template>
 
 <script>
@@ -30,6 +28,7 @@ export default {
     // Log the user in
     login () {
       console.log('Testing method')
+
     }
   }
 }
